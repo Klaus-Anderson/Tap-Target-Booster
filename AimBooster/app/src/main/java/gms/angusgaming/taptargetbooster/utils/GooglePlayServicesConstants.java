0e5a0117377822490293bcf7b4404037feb60d8a
Harry Cliff <harryacliff@gmail.com>
2019-11-16 19:20:32 -0500

    Refactored package structure

--- AimBooster/app/src/main/java/gms/angusgaming/taptargetbooster/utils/GooglePlayServicesConstants.java
+++
@@ -1,5 +0,0 @@
-package gms.angusgaming.taptargetbooster.utils;
-
-public class GooglePlayServicesConstants {
-    public static int RC_SIGN_IN = 9001;
-}
--- AimBooster/app/src/main/java/gms/angusgaming/taptargetbooster/utils/SpawnType.java
+++
@@ -1,7 +0,0 @@
-package gms.angusgaming.taptargetbooster.utils;
-
-public enum SpawnType {
-    RANDOM,
-    INSTANT,
-    TIME
-}
